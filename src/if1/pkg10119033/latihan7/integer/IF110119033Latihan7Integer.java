eaa4446148084b77da815e2510aba8d88eb81245
Dandi Ahmadin <ahmadin.dandi14@gmail.com>
2020-10-16 10:38:34 +0700

    Initial Commit

---
+++ src/if1/pkg10119033/latihan7/integer/IF110119033Latihan7Integer.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119033.latihan7.integer;
+
+/**
+ *
+ * @author 
+ * NAMA     : Dandi Ahmadin
+ * KELAS    : IF-1
+ * NIM      : 10119033
+ * Deskripsi Program : Program ini berisi tipe data bilangan bulat
+ */
+public class IF110119033Latihan7Integer {
+    public static void main(String[] args) {
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int : " + i);
+        System.out.println("long : " + l);
+    }
+}
